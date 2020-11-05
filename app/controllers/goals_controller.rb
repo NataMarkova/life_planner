cf80da3451f31bbd541c739cc413b8d242dac586
natalimarkova <natalia.markova@apomaya.com>
2020-11-05 09:49:07 -0800

    Goal model

--- app/controllers/goals_controller.rb
+++ app/controllers/goals_controller.rb
@@ -18,32 +18,32 @@ class GoalsController < ApplicationController
     end
 
     def create
-        @goal = Goal.new(goal_params)
-        if @goal.save
-          flash.notice = "The goal record was created successfully."
-          redirect_to @goal
-        else
-          flash.now.alert = @goal.errors.full_messages.to_sentence
-          render :new  
-        end
+      @goal = Goal.new(goal_params)
+      if @goal.save
+        flash.notice = "The goal record was created successfully."
+        redirect_to @goal
+      else
+        flash.now.alert = @goal.errors.full_messages.to_sentence
+        render :new  
+      end
     end
 
     def update    
-        if @goal.update(goal_params)
-          flash.notice = "The goal record was updated successfully."
-          redirect_to @goal
-        else
-          flash.now.alert = @goal.errors.full_messages.to_sentence
-          render :edit
-        end
+      if @goal.update(goal_params)
+        flash.notice = "The goal record was updated successfully."
+        redirect_to @goal
+      else
+        flash.now.alert = @goal.errors.full_messages.to_sentence
+        render :edit
+      end
     end
 
     def destroy
-        @goal.destroy
-        respond_to do |format|
-          format.html { redirect_to lgoals_url, notice: 'Goal event was successfully deleted.' }
-          format.json { head :no_content }
-        end
+      @goal.destroy
+      respond_to do |format|
+        format.html { redirect_to lgoals_url, notice: 'Goal event was successfully deleted.' }
+        format.json { head :no_content }
+      end
     end
 
     private
